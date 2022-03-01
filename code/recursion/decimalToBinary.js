9d14d04a251d9a67562c2a7f240a742beb5c5cc3
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-03-01 00:44:44 -0500

    convert decimal to binary recursively

---
+++ code/recursion/decimalToBinary.js
@@ -0,0 +1,16 @@
+answer = [];
+const decimalToBinary = (num) => {
+  if (num === 0) {
+    return 0;
+  }
+  if (num === 1) {
+    answer.unshift(1);
+    return answer;
+  }
+  answer.unshift(num % 2);
+  num -= num % 2;
+  num /= 2;
+  return decimalToBinary(num);
+};
+printAnswer = decimalToBinary(50);
+console.log(...printAnswer);
